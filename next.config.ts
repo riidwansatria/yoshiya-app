668fe74b244789556a84e61b1ce9789a756749e4
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2026-04-05 16:12:22 +0900

    fix: build errors

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
-};
+} satisfies NextConfig;
 
 export default withNextIntl(nextConfig);
-
